fe35774b360c3a937bbd4cb92a32acf335e873ff
Hrvoje Milković <hmilkovi@gmail.com>
2015-05-10 23:25:37 +0200

    app

---
+++ AEES Desktop 1.0.0.0/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace AEES_Desktop_1._0._0._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pronađi prazne kružiće i izbriši";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Otvori sliku";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(158, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(396, 590);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(573, 46);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(422, 590);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(1017, 46);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(391, 590);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(154, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Učitana slika";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(569, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(294, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Pronađeni prazni kružići i izbrisani";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(1013, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(243, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Pronađeni samo crni kružići";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(12, 277);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 40);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Pronađi samo crne kružiće";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1420, 648);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "AEES Destop verzija 1.0.0.0.";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ AEES Desktop 1.0.0.0/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AForge;
+using AForge.Math;
+using AForge.Imaging;
+using AForge.Math.Geometry;
+using AForge.Imaging.Filters;
+
+
+using Image = System.Drawing.Image; //Remove ambiguousness between AForge.Image and System.Drawing.Image
+using Point = System.Drawing.Point; //Remove ambiguousness between AForge.Point and System.Drawing.Point
+
+namespace AEES_Desktop_1._0._0._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static Bitmap bitmap, crni;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Open Image";
+            dlg.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            dlg.Multiselect = false;
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                // Set the size of the PictureBox control. 
+                this.pictureBox1.Size = new System.Drawing.Size(396, 590);
+
+                //Set the SizeMode to center the image. 
+                this.pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+
+                // Set the border style to a three-dimensional border. 
+                this.pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+
+
+                //Load picture
+                pictureBox1.Image = new Bitmap(dlg.FileName);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                bitmap = new Bitmap(dlg.FileName) as Bitmap;
+            }
+            button1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap temp = bitmap;
+            // lock image
+            BitmapData bitmapData = temp.LockBits(
+                new Rectangle(0, 0, temp.Width, temp.Height),
+                ImageLockMode.ReadWrite, temp.PixelFormat);
+
+            // step 1 - turn background to black
+            ColorFiltering colorFilter = new ColorFiltering();
+
+            colorFilter.Red   = new IntRange( 0, 64 );
+            colorFilter.Green = new IntRange( 0, 64 );
+            colorFilter.Blue  = new IntRange( 0, 64 );
+            colorFilter.FillOutsideRange = false;
+
+            colorFilter.ApplyInPlace( bitmapData );
+
+            // step 2 - locating objects
+            BlobCounter blobCounter = new BlobCounter();
+
+            blobCounter.FilterBlobs = true;
+            blobCounter.MinHeight = 5;
+            blobCounter.MinWidth = 5;
+
+            blobCounter.ProcessImage(bitmapData);
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+            temp.UnlockBits(bitmapData);
+
+            // step 3 - check objects' type and highlight
+            SimpleShapeChecker shapeChecker = new SimpleShapeChecker();
+
+            Graphics g = Graphics.FromImage(temp);
+            Pen pen = new Pen(Color.White, 30);
+
+            for (int i = 0, n = blobs.Length; i < n; i++)
+            {
+                List<IntPoint> edgePoints = blobCounter.GetBlobsEdgePoints(blobs[i]);
+
+                AForge.Point center;
+                float radius;
+
+                if (shapeChecker.IsCircle(edgePoints, out center, out radius))
+                {
+                    g.DrawEllipse(pen,
+                        (int)(center.X - radius),
+                        (int)(center.Y - radius),
+                        (int)(radius * 2),
+                        (int)(radius * 2));
+                }
+            }
+
+            g.Dispose();
+            pen.Dispose();
+
+            // Set the size of the PictureBox control. 
+            this.pictureBox2.Size = new System.Drawing.Size(396, 590);
+
+            //Set the SizeMode to center the image. 
+            this.pictureBox2.SizeMode = PictureBoxSizeMode.CenterImage;
+
+            // Set the border style to a three-dimensional border. 
+            this.pictureBox2.BorderStyle = BorderStyle.Fixed3D;
+
+            pictureBox2.Image = temp;
+            crni = temp;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            button3.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            Bitmap orig = crni;
+
+            Bitmap clone = new Bitmap(orig.Width, orig.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            using (Graphics gr = Graphics.FromImage(clone))
+            {
+                gr.DrawImage(orig, new Rectangle(0, 0, clone.Width, clone.Height));
+            }
+
+            FiltersSequence commonSeq = new FiltersSequence();
+            commonSeq.Add(Grayscale.CommonAlgorithms.BT709);
+            commonSeq.Add(new BradleyLocalThresholding());
+            commonSeq.Add(new DifferenceEdgeDetector());
+
+            Bitmap temp = commonSeq.Apply(clone);
+
+            BlobsFiltering filter = new BlobsFiltering();
+            filter.CoupledSizeFiltering = true;
+            filter.MinWidth = 10;
+            filter.MinHeight = 10;
+            // apply the filter
+            filter.ApplyInPlace(temp);
+
+            BlobCounter blobCounter = new BlobCounter();
+            blobCounter.ProcessImage(temp);
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+
+            // step 3 - check objects' type and highlight
+            SimpleShapeChecker shapeChecker = new SimpleShapeChecker();
+            Bitmap temp4 = (Bitmap)pictureBox1.Image;
+            Graphics g = Graphics.FromImage(temp4);
+            Pen pen = new Pen(Color.Red, 2);
+
+            for (int i = 0, n = blobs.Length; i < n; i++)
+            {
+                List<IntPoint> edgePoints = blobCounter.GetBlobsEdgePoints(blobs[i]);
+
+                AForge.Point center;
+                float radius;
+
+                if (shapeChecker.IsCircle(edgePoints, out center, out radius))
+                {
+                    g.DrawEllipse(pen,
+                        (int)(center.X - radius),
+                        (int)(center.Y - radius),
+                        (int)(radius * 2),
+                        (int)(radius * 2));
+                }
+            }
+
+            g.Dispose();
+            pen.Dispose();
+
+            // Set the size of the PictureBox control. 
+            this.pictureBox3.Size = new System.Drawing.Size(396, 590);
+
+            //Set the SizeMode to center the image. 
+            this.pictureBox3.SizeMode = PictureBoxSizeMode.CenterImage;
+
+            // Set the border style to a three-dimensional border. 
+            this.pictureBox3.BorderStyle = BorderStyle.Fixed3D;
+
+            pictureBox3.Image = temp4;
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
